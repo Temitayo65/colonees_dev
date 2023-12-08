08027a60f302a273d22430ccb8d5c8415cce1838
Mac <mac@Macs-MacBook-Pro-2.local>
2023-12-09 00:57:24 +0100

    Add Installation Requirements file

--- app/main.py
+++ app/main.py
@@ -3,11 +3,22 @@ from . import models
 from fastapi import FastAPI
 from .database import engine
 from .config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["*"] # put only http://www.colonees.com here 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins= origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(subscribe.router)
 app.include_router(business.router)
