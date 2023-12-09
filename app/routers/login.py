d5d7d532959a659c146810b5b6089d9c8f4be32c
temitayo65 <iloritemitayo65@gmail.com>
2023-12-09 05:41:02 +0100

    Creating Response Model for getting talent-users

--- app/routers/login.py
+++ app/routers/login.py
@@ -1,9 +1,9 @@
-from typing import Optional
+from typing import List, Optional
 from .. import schemas, models, utils, oauth2
 from fastapi.params import Depends
 from ..database import get_db
 from sqlalchemy.orm import Session 
-from fastapi import APIRouter
+from fastapi import security, security, security, security, APIRouter
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from fastapi import APIRouter, HTTPException,status
 from ..models import User
@@ -30,7 +30,7 @@ async def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Ses
     return {"access_token": access_token, "token_type": "bearer"}
 
 
-@router.get("/talent-users")
+@router.get("/talent-users", response_model=schemas.TalentProperUserResponse)
 def get_users(db:Session = Depends(get_db), current_user: str=Depends(oauth2.get_current_user),limit: int = 3, skip: int = 0, search: Optional[str] = ""):
     if not current_user.is_authenticated:
         raise HTTPException(
@@ -39,4 +39,17 @@ def get_users(db:Session = Depends(get_db), current_user: str=Depends(oauth2.get
         )
     users = db.query(models.TalentUser).filter(models.TalentUser.role.contains(search)).limit(limit).offset(skip).all()
     [user.__dict__.pop("password") for user in users] # this gets each dictionary from the "users" table and removes the password before returning the response 
-    return users # this does not contain the passwords
+    talent_users = [
+        schemas.TalentProperUserResponse(
+            first_name=user.first_name,
+            last_name=user.last_name,
+            email=user.email,
+            phone_number=user.phone_number,
+            role=user.role,
+            subscribed=user.subscribed,
+            portfolio_link=user.portfolio_link
+        )
+        for user in users
+    ]
+    
+    return talent_users # this does not contain the passwords
