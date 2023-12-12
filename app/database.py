aba7ab225f8a8f7f4de3c7347c656e0c61574f59
temitayo65 <iloritemitayo65@gmail.com>
2023-12-12 14:39:37 +0100

    Create initial admin user in database

--- app/database.py
+++ app/database.py
@@ -2,6 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
+from . import utils, models
+
+
 
 
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
@@ -10,11 +13,24 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
+def initialize_admin_user(db_session):
+    # Check if the admin user exists
+    admin_user = db_session.query(models.AdminUser).filter_by(
+        models.AdminUser.email =="iloritemitayo75@gmail.com").first()
+
+    if not admin_user:
+        # Create the admin user
+        admin_user = models.AdminUser(email="iloritemitayo75@gmail.com",
+                          password=utils.hash(settings.database_password), is_admin=True)
+        db_session.add(admin_user)
+        db_session.commit()
+
 def get_db():
     db = SessionLocal()
     try: 
+        initialize_admin_user(db)
         yield db 
     finally:
         db.close()
 
-
+    
