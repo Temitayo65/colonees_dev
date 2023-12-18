bb25fa7360c2e97d0bd425eebb033ccb1e14c80c
temitayo65 <iloritemitayo65@gmail.com>
2023-12-18 23:22:21 +0100

    Include endpoint to give admin rights

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -32,7 +32,7 @@ async def login(admin_user_credentials: OAuth2PasswordRequestForm = Depends(), d
 
 
 @router.get("/all-users", response_model=List[schemas.UserResponse])
-def get_all_users(db: Session = Depends(get_db), current_user: str = Depends(oauth2.get_current_user)):
+async def get_all_users(db: Session = Depends(get_db), current_user: str = Depends(oauth2.get_current_user)):
     if not current_user.is_authenticated:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -52,3 +52,42 @@ def get_all_users(db: Session = Depends(get_db), current_user: str = Depends(oau
 
     return all_users  
 
+
+@router.post("/admin-rights")
+async def give_admin_rights(newAdminUser: schemas.AdminUser, current_user: str = Depends(oauth2.get_current_user), db: Session = Depends(get_db)):
+    if not current_user.is_authenticated:
+       raise HTTPException(
+           status_code=status.HTTP_401_UNAUTHORIZED,
+           detail="Authentication required"
+       )
+    
+    # check to see if the user already exists 
+    existing_user = db.query(models.AdminUser).filter(
+        models.AdminUser.email == newAdminUser.email).first()
+    if existing_user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=f"User with email: {newAdminUser.email} already exists")
+    
+    # check to see if the current user is a master Admin user(check this state in the database)
+    is_master_user = db.query(models.AdminUser).filter(models.AdminUser.email == current_user.email).first()
+    
+    if is_master_user.is_master:
+        # add to admin table
+        try:
+            user = models.AdminUser(**newAdminUser.model_dump())
+            user.password = utils.hash(newAdminUser.password)
+            db.add(user)
+            db.commit()
+            db.refresh(user)
+            return {"message": f"user with email: {user.email}, successfully added as Admin"}
+        
+        except sqlalchemy.exc.IntegrityError as e:
+            db.rollback()
+            if "duplicate key" in str(e):
+                raise HTTPException(status_code=status.HTTP_302_FOUND,
+                                    detail=f"{user.business_name} is already an Admin")
+            else:
+                raise HTTPException(
+                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal server error")
+
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Only master admins can make others admin")
