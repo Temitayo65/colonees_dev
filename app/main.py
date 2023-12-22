0e9447eda0820824acef82f69efd5188c48a05ca
Ameer Adeigbe <ameeradeigbe@gmail.com>
2023-12-23 00:38:46 +0100

    Bug Fiz by Ameer

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from . import models
 from fastapi import FastAPI
 from .database import engine
 from .config import settings
+from fastapi.middleware import Middleware
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
