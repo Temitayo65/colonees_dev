51d9a4eb43fd2cf6f442db4cf5d85d396a331c51
Olaoluwa Okikiade <devlaolu>
2023-12-28 19:16:13 +0100

    db ssl mode

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ from . import utils, models
 
 
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}?sslmode=require'
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
