ee08c40693c8eb041c9e254e5a372ffc8032acbd
Olaoluwa Okikiade <devlaolu>
2023-12-28 20:02:30 +0100

    db ssl mode

--- app/database.py
+++ app/database.py
@@ -3,12 +3,13 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
 from . import utils, models
+import ssl
 
 
 
-
+ssl_context = ssl.SSLContext()
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'ssl_context': True},echo=True,)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'ssl_context': ssl_context} ,echo=True,)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
